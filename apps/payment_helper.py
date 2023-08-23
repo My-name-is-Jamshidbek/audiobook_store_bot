4c7af9702daea30b4ab92e823f1d34ad862276db
xjama <jamshidbekollanazarov@outlook.com>
2023-08-23 19:38:17 +0000

    serverdan

--- apps/payment_helper.py
+++ apps/payment_helper.py
@@ -38,33 +38,33 @@ async def on_callback_query(callback_query: CallbackQuery):
                     payload=f"{book_name}_{callback_query.data.split('_')[-1]}",
                 )
             
-    elif callback_query.data.startswith("payme_"):
-        book_id = callback_query.data.split("_")[1]
-        book_name = get_premium_book_name_by_id(book_id)
-        if callback_query.data.split('_')[-1] == "e":
-            await bot.send_invoice(
-                    callback_query.from_user.id,
-                    title=f"{book_name}",
-                    description=f"Payme orqali to'lov",
-                    provider_token=PAYME_TOKEN,  # Payme taminotchisining tokeni
-                    currency="uzs",
-                    is_flexible=False,
-                    prices=[get_price_label(f"{book_name}", int(get_premium_book_price_by_id(book_id)))],
-                    start_parameter="premium-book-subcription",
-                    payload=f"{book_name}_{callback_query.data.split('_')[-1]}",
-                )
-        elif callback_query.data.split('_')[-1] == "a":
-                await bot.send_invoice(
-                    callback_query.from_user.id,
-                    title=f"{book_name}",
-                    description=f"Payme orqali to'lov",
-                    provider_token=PAYME_TOKEN,  # Payme taminotchisining tokeni
-                    currency="uzs",
-                    is_flexible=False,
-                    prices=[get_price_label(f"{book_name}", int(get_premium_audiobook_price_by_id(book_id)))],
-                    start_parameter="premium-book-subcription",
-                    payload=f"{book_name}_{callback_query.data.split('_')[-1]}",
-                )
+    elif callback_query.data.startswith("payme_"):await bot.send_message(callback_query.from_user.id, "Payme orqali to'lov profilaktika sababli vaqtincha mavjud emas!")
+#        book_id = callback_query.data.split("_")[1]
+ #       book_name = get_premium_book_name_by_id(book_id)
+  #      if callback_query.data.split('_')[-1] == "e":
+   #         await bot.send_invoice(
+    #                callback_query.from_user.id,
+     #               title=f"{book_name}",
+      #              description=f"Payme orqali to'lov",
+       #             provider_token=PAYME_TOKEN,  # Payme taminotchisining tokeni
+        #            currency="uzs",
+         #           is_flexible=False,
+          #          prices=[get_price_label(f"{book_name}", int(get_premium_book_price_by_id(book_id)))],
+           #         start_parameter="premium-book-subcription",
+            #        payload=f"{book_name}_{callback_query.data.split('_')[-1]}",
+             #   )
+#        elif callback_query.data.split('_')[-1] == "a":
+#                await bot.send_invoice(
+ #                   callback_query.from_user.id,
+  #                  title=f"{book_name}",
+   #                 description=f"Payme orqali to'lov",
+    #                provider_token=PAYME_TOKEN,  # Payme taminotchisining tokeni
+     #               currency="uzs",
+      #              is_flexible=False,
+       #             prices=[get_price_label(f"{book_name}", int(get_premium_audiobook_price_by_id(book_id)))],
+        #            start_parameter="premium-book-subcription",
+         #           payload=f"{book_name}_{callback_query.data.split('_')[-1]}",
+          #      )
                 
     elif callback_query.data.startswith("visa_"):
         book_id = callback_query.data.split("_")[1]
